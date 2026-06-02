cd8e2916a1a2f7fdf74a1f51a38bcd51b9518d48
Arzza Munabim <aprilabim2804@gmail.com>
2026-06-03 00:22:51 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <h1>Flet Goo</h1>
+  <p>
+    Hello World! This is a Vue 3 + Vite + TypeScript + Tailwind CSS template. You can start building your app from here.
+  </p>
+</template>
+
+<style scoped></style>
